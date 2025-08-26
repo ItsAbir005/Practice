b89cb2aa9a8c8bf1f2018bd1d894b3cfb6c56788
Abir Maity <abirmaity44@gmail.com>
2025-08-26 19:10:28 +0530

    chat

--- server.js
+++ server.js
@@ -1,32 +1,23 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 const http = require('http');
-const { Server } = require("socket.io");
-
+const { Server } = require('socket.io');
 const server = http.createServer(app);
 const io = new Server(server);
-
+const PORT = 3000;
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client.html');
+});
 io.on('connection', (socket) => {
-  console.log('Client connected', socket.id);
-
-  socket.on("ping", (data) => {
-    console.log("ping received", data);
-    io.emit("pong", { 
-      from: socket.id, 
-      message: "pong from server to everyone" 
-    });
+  console.log('Client connected:', socket.id);
+  socket.on('chat message', (msg) => {
+    console.log('Message received:', msg);
+    io.emit('chat message', msg); 
   });
-
   socket.on('disconnect', () => {
-    console.log('user disconnected', socket.id);
+    console.log('Client disconnected:', socket.id);
   });
 });
-
-app.get("/", (req, res) => {
-  res.send("Hello from Express + Socket.io server!");
-});
-
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
