272fd4b8cf70b1ccbaf8af99bdfa28bb31eb9050
Abir Maity <abirmaity44@gmail.com>
2025-08-28 18:55:59 +0530

    change

--- server.js
+++ server.js
@@ -1,23 +1,6 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const { Server } = require('socket.io');
-const server = http.createServer(app);
-const io = new Server(server);
-const PORT = 3000;
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/client.html');
-});
-io.on('connection', (socket) => {
-  console.log('Client connected:', socket.id);
-  socket.on('chat message', (msg) => {
-    console.log('Message received:', msg);
-    io.emit('chat message', msg); 
-  });
-  socket.on('disconnect', () => {
-    console.log('Client disconnected:', socket.id);
-  });
-});
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
