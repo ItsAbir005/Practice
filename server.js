917384f455e69d3a83767b9297650e92cd9b085c
Abir Maity <abirmaity44@gmail.com>
2025-09-06 00:20:53 +0530

    payment

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
 const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
-const { gql } = require("apollo-server-express");
-const bodyParser = require("body-parser");
-const typeDefs = require("./schema");
-const resolvers = require("./resolvers");
-const mongoose = require("mongoose");
-const uri = "mongodb+srv://maityabir040:s03qu2Z4oA8IMgLs@cluster0.o9ynclx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
-async function startServer() {
-  const app = express();
-  
-  const server = new ApolloServer({ typeDefs, resolvers });
-  await server.start();
-  server.applyMiddleware({ app });
-  app.listen({ port: 4000 }, () => {
-    console.log("Server is running on http://localhost:4000/graphql");
-  });
-}
-startServer();
+const dotenv = require("dotenv");
+const Stripe = require("stripe");
+dotenv.config();
+const app = express();
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+app.post("/donate", async (req, res) => {
+  const { amount, currency } = req.body;
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: [
+        {
+          price_data: {
+            currency: currency || "usd",
+            product_data: {
+              name: "Donation",
+            },
+            unit_amount: amount,
+          },
+          quantity: 1,
+        },
+      ],
+      success_url: "https://example.com/success",
+      cancel_url: "https://example.com/cancel",
+    });
+
+    res.status(200).json({ url: session.url });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
